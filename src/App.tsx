ea8be88b9776366120baa8b199962d689ebde5e5
carolmmv <carolmmv@hotmail.com>
2021-10-04 22:15:00 -0300

    adicionando arquivo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { StylesProvider } from '@material-ui/styles';
+import { ThemeProvider as MuiProvider } from '@material-ui/core';
+import { persistor, store } from 'state/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ptBR } from '@material-ui/core/locale';
+import Routes from 'routes';
+import theme from 'styles/theme';
+import GlobalStyle from 'styles/global';
 import './App.css';
+
+
+const muiTheme = createTheme({
+  palette: {
+    primary: { main: theme.colors.primary },
+    secondary: { main: theme.colors.secondary },
+    error: { main: theme.colors.error },
+  },
+  typography: {
+    fontFamily: theme.fonts.primary,
+    button: {
+      fontSize: 16
+    }
+  }
+}, ptBR);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={muiTheme}>
+          <ThemeProvider theme={theme}>
+            <StylesProvider injectFirst>
+              <MuiProvider theme={muiTheme}>
+                <GlobalStyle />
+                <Routes />
+              </MuiProvider>
+            </StylesProvider>
+          </ThemeProvider>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
