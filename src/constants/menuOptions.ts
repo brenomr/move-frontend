4f8d62181865c41febb99462367987ac19fd7206
carolmmv <carolmmv@hotmail.com>
2021-10-04 22:17:46 -0300

    adicionando opções do menu

---
+++ src/constants/menuOptions.ts
@@ -0,0 +1,10 @@
+import { Person } from "@material-ui/icons";
+import { ISideMenuOption } from "components/SideMenu/index.d";
+
+const menuOptions: ISideMenuOption[] = [
+    { icon: Person, label: 'Meu Perfil', link: '/perfil' },
+    { icon: Person, label: 'Meu Perfil', link: '/perfil' },
+    { icon: Person, label: 'Meu Perfil', link: '/perfil' }
+]
+
+export default menuOptions;
