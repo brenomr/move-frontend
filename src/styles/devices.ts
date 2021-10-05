1eb95a5725badd22994418a13d83d6f910b87eee
carolmmv <carolmmv@hotmail.com>
2021-10-04 22:15:07 -0300

    adicionando módulo de estilo

---
+++ src/styles/devices.ts
@@ -0,0 +1,20 @@
+/**
+ * I am a device list.
+ */
+ const devices = {
+    mobileS: '(max-width: 320px)',
+    mobileM: '(max-width: 375px)',
+    mobileL: '(max-width: 425px)',
+    tablet: '(max-width: 768px)',
+    laptop: '(max-width: 1024px)',
+    laptopL: '(max-width: 1280px)',
+    desktop: '(max-width: 1366px)',
+    desktopM: '(max-width: 1635px)',
+    desktopFull: '(max-width: 1920px)',
+  };
+  
+  
+  /**
+   * EXPORTS
+   */
+  export { devices };  
---
+++ src/styles/theme.ts
@@ -0,0 +1,52 @@
+/**
+ * I am color palette.
+ */
+export const colors = {
+  white: "#ffffff",
+  almostWhite: "#F6F6F6",
+  grayLight: "#ededed",
+  gray: "#c1c1c1",
+  grayDark: "#292929",
+  black: "#191919",
+  error: "#e25656",
+  success: "#74c174",
+  primary: "#ffc65c",
+  primaryLight: "#ffe8bd",
+  primaryDark: "#e1b35d",
+  secondary: "#ff5c6b",
+  secondaryDark: "#b64751"
+}
+
+const gradients = {
+  primary: `linear-gradient(to top, ${colors.secondary}, ${colors.secondaryDark})`,
+};
+
+/**
+ * I am a fonts list.
+ */
+const fonts = {
+  weight: {
+    bold: 500,
+    bolder: 700,
+    medium: 400,
+    light: 300,
+    extraLight: 200,
+  },
+  primary: 'Roboto, sans-serif',
+  secondary: '"Montserrat", "Red Hat Display"'
+};
+
+/**
+ * I am an application theme.
+ */
+const theme = {
+  fonts: { ...fonts },
+  colors: colors,
+  gradients: gradients,
+  background: colors.grayDark
+};
+
+/**
+ * EXPORTS
+ */
+export default theme;
