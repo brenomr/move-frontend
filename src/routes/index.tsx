14d2517f9c79d81fbb72a9ca4f46a9319dba6e4a
carolmmv <carolmmv@hotmail.com>
2021-10-05 22:33:12 -0300

    adicionando rota de aluno e not found

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,31 @@
 import { selectUser } from 'features/user/selectors';
+import MainLayout from 'layout/main';
 import HomePage from 'pages/home';
 import LoginPage from 'pages/login';
+import StudentsList from 'pages/students/list';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFoundImage from 'assets/images/notfound.svg';
 
 const Routes = () => {
 
     const { signed } = useSelector(selectUser);
 
+    const NotFound = () => <img src={NotFoundImage} alt="Página não econtrada" />
     return (
         <BrowserRouter>
             {signed ?
-                <>
-                    <Route path="/" exact component={HomePage} />
-                </>
+                <MainLayout>
+                    <Switch>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/alunos" exact component={StudentsList} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </MainLayout>
                 :
                 <Switch>
                     <Route path="/" exact component={LoginPage} />
+                    <Route component={NotFound} />
                 </Switch>
             }
         </BrowserRouter>
