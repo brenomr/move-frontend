4df4092ef09ca0bc7fa16fbaeed26bce22ac8700
carolmmv <carolmmv@hotmail.com>
2021-10-11 18:49:12 -0300

    adicionando modulo de atividade

---
+++ src/pages/activities/create/index.tsx
@@ -0,0 +1,185 @@
+import React, { useState, useEffect, FormEvent, useCallback } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import { TextField } from '@material-ui/core';
+import Button from 'components/Button';
+import BackIcon from '@material-ui/icons/ArrowBack';
+
+import Swal from 'sweetalert2';
+
+import { selectUser } from 'features/user/selectors';
+import { useSelector } from 'react-redux';
+import { endpoints } from "constants/endpoints"
+
+import useStyles from 'styles/styles';
+import api from 'services/api';
+import responseCheck from 'utils/responseCheck';
+import Paper from 'components/Paper';
+import { SaveOutlined } from '@material-ui/icons';
+import { namings } from 'constants/namings';
+
+interface ParamTypes {
+    id: string;
+}
+
+
+function CreateActivity() {
+    const [id, setId] = useState('');
+    const params = useParams<ParamTypes>();
+
+    const { id: personalId } = useSelector(selectUser);
+
+    const initialState = {
+        id: undefined,
+        name: '',
+        category: '',
+        image_url: 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=',
+        user: { id: personalId }
+    };
+    const [state, setState] = useState(initialState);
+    const handleChange = (event: React.ChangeEvent<any>) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [phoneError, setPhoneError] = useState(false);
+
+    const [disableButton, setDisableButton] = useState(false);
+    const [isNew, setIsNew] = useState(false);
+
+    const classes = useStyles();
+    const history = useHistory();
+
+    const getIsNew = useCallback(() => {
+        if (!id) {
+            if (params.id) {
+                setId(params.id);
+            } else {
+                setIsNew(true);
+            }
+        }
+    }, [id, params.id]);
+
+    useEffect(() => {
+        if (!isNew) {
+            if (id) {
+                (async () => {
+                    const response = await api.get(`/${endpoints.activities}/${id}`);
+                    if (responseCheck(response)) {
+                        const data = await response.json();
+                        setState(data);
+                        setId(data.id);
+                    }
+                    else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Erro ao carregar a página',
+                            text: 'Verifique se o registro consultado realmente existe, caso o erro persista contate o suporte técnico.'
+                        }).then(() => {
+                            history.push(`/${endpoints.activities}`);
+                        });
+                    }
+                })();
+            }
+        }
+        getIsNew();
+    }, [id, isNew, getIsNew, history]);
+
+
+    async function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        try {
+            let result;
+            setDisableButton(true);
+
+            if (isNew) {
+                result = await api.post(`/${endpoints.activities}`, state);
+            }
+
+            else {
+                result = await api.put(`/${endpoints.activities}/${id}`, state);
+            }
+
+            if (!responseCheck(result)) {
+                throw Error;
+            }
+            Swal.fire({
+                title: `${namings.trainings.singular} ${isNew ? 'cadastrado' : 'editado'} com sucesso!`,
+                icon: 'success',
+                showCloseButton: true,
+                showCancelButton: true,
+                focusConfirm: false,
+                confirmButtonText: 'Continuar',
+                cancelButtonColor: '#418107',
+                cancelButtonText: 'Ver Todos'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    if (isNew)
+                        setState(initialState);
+                }
+                else {
+                    history.goBack();
+                }
+            });
+        } catch (error) {
+            Swal.fire({
+                title: 'Não foi possível salvar',
+                text: 'Tente novamente, caso o erro persista contate o suporte técnico.',
+                icon: 'error',
+            });
+        } finally {
+            setDisableButton(false);
+        }
+    }
+
+    return (
+        <Paper>
+            <h1>{isNew ? 'Cadastrar' : 'Editar'} {namings.activities.singular}</h1>
+            <form onSubmit={handleSubmit}>
+                <div className={classes.grid}>
+                    <TextField
+                        label="Nome"
+                        variant="outlined"
+                        required
+                        name="name"
+                        value={state.name}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Categoria"
+                        variant="outlined"
+                        required
+                        name="category"
+                        value={state.category}
+                        onChange={handleChange}
+                    />
+
+                </div>
+                <div className={classes.button}>
+                    <Button
+                        color="secondary"
+                        type="button"
+                        icon={<BackIcon />}
+                        onClick={() => { history.goBack() }}
+                    >
+                        Voltar
+                    </Button>
+                    <Button
+                        loading={disableButton}
+                        icon={<SaveOutlined />}
+                        type="submit"
+                    >
+                        Salvar
+                    </Button>
+                </div>
+            </form>
+        </Paper>
+    )
+}
+
+export default CreateActivity;
---
+++ src/pages/activities/list/index.tsx
@@ -0,0 +1,86 @@
+import { Add, Delete, Edit } from "@material-ui/icons"
+import { IRow } from "components/EnhancedTable/index.d"
+import { Table } from "components/EnhancedTable"
+import { ICell } from "components/EnhancedTable/Header/index.d"
+import { endpoints } from "constants/endpoints"
+import { links } from "constants/links"
+import { namings } from "constants/namings"
+import useData from "hooks/useData"
+import { useState } from "react"
+import api from "services/api"
+import Swal from "sweetalert2"
+
+const ActivitiesList = () => {
+    const base = endpoints.activities;
+    const [orderBy, setOrderBy] = useState<keyof ICell>('name');
+    const [loading, setLoading] = useState(true);
+    const [data, setParams, update] = useData(base, setLoading);
+
+    const handleDelete = async (id: string | null) => {
+
+        const result = await Swal.fire({
+            title: `Você tem certeza que deseja excluir este ${namings.activities.singular}`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: "#418107",
+            confirmButtonText: "Sim, excluir",
+            cancelButtonText: "Cancelar"
+        })
+        if (result.isConfirmed) {
+            try {
+                await api.delete(`/${endpoints.activities}/${id}`);
+                update();
+            } catch (error) {
+                Swal.fire({
+                    title: 'Não foi possível excluir',
+                    text: 'Tente novamente, caso o erro persista contate o suporte técnico.',
+                    icon: 'error',
+                });
+            }
+        }
+    }
+
+    return (
+        <Table
+            title={namings.activities.plural}
+            actions={[
+                {
+                    name: `Cadastrar ${namings.activities.singular}`,
+                    icon: <Add />,
+                    link: `${links.activities}/cadastrar`
+                }
+            ]}
+            orderBy={orderBy}
+            loading={loading}
+            rows={data}
+            options={[
+                {
+                    type: 'link',
+                    name: 'Editar',
+                    icon: <Edit />,
+                    link: `${links.activities}/`,
+                    handle: () => { }
+                },
+                {
+                    type: 'button',
+                    name: 'Excluir',
+                    icon: <Delete />,
+                    handle: handleDelete
+                }
+            ]}
+            selectedCells={(value: IRow) => {
+                return {
+                    id: value.id,
+                    name: value.name,
+                    category: value.category
+                }
+            }}
+            cells={[
+                { id: 'name', label: 'Nome' },
+                { id: 'category', label: 'Categoria' }
+            ]}
+        />
+    )
+}
+
+export default ActivitiesList;
