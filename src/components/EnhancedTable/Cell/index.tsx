60f53b109035f6107558d407fd6a7f93c7805d93
carolmmv <carolmmv@hotmail.com>
2021-10-27 00:42:44 -0300

    criando limite de tamanho para imagem nas tabelas

--- src/components/EnhancedTable/Cell/index.tsx
+++ src/components/EnhancedTable/Cell/index.tsx
@@ -1,13 +1,22 @@
 import { TableCell } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
+
+const XTableCell = styled(TableCell)`
+  img{
+    max-height: 200px;
+    max-width: 400px;
+    border-radius: 20px;
+  }
+`
 
 export default function Cell(data: any, index: number) {
   return (
     <>
-      {index !== 0 && 
-        <TableCell key={index} align="left">
+      {index !== 0 &&
+        <XTableCell key={index} align="left">
           {data}
-        </TableCell>
+        </XTableCell>
       }
     </>
   )
