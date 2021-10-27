f4a62ecc9e4ab068ed2ba4e44eb22d980108e65a
carolmmv <carolmmv@hotmail.com>
2021-10-27 00:41:51 -0300

    editando arquivo da api para suportar body type form

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,15 +8,20 @@ export class API {
     this.baseURL = baseURL || process.env.REACT_APP_API_HOST || 'http://localhost:3000';
   }
 
-  private async processRequest(verb: string, url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) {
+  private async processRequest(verb: string, url: string, data?: any, bodyForm?: boolean, headers?: { [key: string]: string }, useAuth: boolean = true) {
 
     try {
-      const response = await fetch(joinURL(this.baseURL, url), {
-        method: verb,
-        ...(data ? { body: JSON.stringify(data) } : {}),
-        headers: {
+      if (!bodyForm)
+        headers = {
           ...headers,
           'Content-Type': 'application/json;charset=utf-8',
+        }
+
+      const response = await fetch(joinURL(this.baseURL, url), {
+        method: verb,
+        ...(data ? bodyForm ? { body: data } : { body: JSON.stringify(data) } : {}),
+        headers: {
+          ...headers,
           'Authorization': 'Bearer ' + localStorage.getItem('token')
         },
         mode: 'cors'
@@ -56,11 +61,11 @@ export class API {
     }
   }
 
-  put = (url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('PUT', url, data, headers, useAuth);
-  get = (url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('GET', url, data, headers, useAuth);
-  post = (url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('POST', url, data, headers, useAuth);
-  patch = (url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('PATCH', url, data, headers, useAuth);
-  delete = (url: string, data?: { [key: string]: any }, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('DELETE', url, data, headers, useAuth);
+  put = (url: string, data?: any, bodyForm?: boolean, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('PUT', url, data, bodyForm, headers, useAuth);
+  get = (url: string, data?: any, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('GET', url, data, false, headers, useAuth);
+  post = (url: string, data?: any, bodyForm?: boolean, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('POST', url, data, bodyForm, headers, useAuth);
+  patch = (url: string, data?: any, bodyForm?: boolean, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('PATCH', url, data, bodyForm, headers, useAuth);
+  delete = (url: string, data?: any, headers?: { [key: string]: string }, useAuth: boolean = true) => this.processRequest('DELETE', url, data, false, headers, useAuth);
 }
 
 export default new API();
